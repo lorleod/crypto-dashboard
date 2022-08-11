fb712e6797cb2df1ac321e1191ef9e2cde4c4358
Taylor McLeod <Taylor@xyloMac.local>
2022-08-10 17:25:50 -0700

    found api call - trying it

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import './App.css';
 
 function App() {
 
+  // on load, get request prices last 7 days from coingecko
   useEffect(() => {
     const fetchData = () => {
-      axios.get(`https://api.coingecko.com/api/v3/coins/bitcoin/history?date=06-08-2021&localization=false`)
+      axios.get(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=cad&days=7&interval=daily`)
         .then((response) => {
-          console.log(response);
+          console.log(response.data);
+          let sevendayprices = [];
+          for (let price of response.data.prices) {
+            sevendayprices.push(price[1]);
+          }
+          console.log(sevendayprices);
         })
         .catch((error) => {
           console.log("error:", error)
