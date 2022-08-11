6f8cc9ee20b4bced8166d1d596653a5d1ddb6d37
Taylor McLeod <Taylor@xyloMac.local>
2022-08-10 17:34:15 -0700

    generate array of dates of last 7 days

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ function App() {
 
   // on load, get request prices last 7 days from coingecko
   useEffect(() => {
+
+    // create array of dates of last 7 days
+    const pastSevenDays = [...Array(7).keys()].map(index => {
+      const date = new Date();
+      date.setDate(date.getDate() - index);
+
+      return date;
+    });
+
+    console.log("pastSevenDays: ", pastSevenDays);
+
     const fetchData = () => {
       axios.get(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=cad&days=7&interval=daily`)
         .then((response) => {
@@ -15,6 +26,7 @@ function App() {
             sevendayprices.push(price[1]);
           }
           console.log(sevendayprices);
+
         })
         .catch((error) => {
           console.log("error:", error)
