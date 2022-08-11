f9eb79b175055ef45be581d11f1fb5fd7ed20018
Taylor McLeod <Taylor@xyloMac.local>
2022-08-10 17:01:08 -0700

    testing coingecko api response

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios.get(`https://api.coingecko.com/api/v3/coins/bitcoin/history?date=06-08-2021&localization=false`)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log("error:", error)
+        });
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ src/table.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function table (props) {
+
+
+
+  return (
+    <div>
+
+    </div>
+  );
+}
